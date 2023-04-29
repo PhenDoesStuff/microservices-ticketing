24e2e54a219df0a58fab22b8d7bd4d340fb925dd
Stephen Montague <Stephen.D.Montague@gmail.com>
2023-04-29 12:17:55 -0400

    Added error validation middlewares and new objects

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,18 +1,21 @@
 import express from 'express';
 import { json } from 'body-parser';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
+import { currentUserRouter } from './routes/currentUser';
+import { signInRouter } from './routes/signIn';
+import { signOutRouter } from './routes/signOut';
+import { signUpRouter } from './routes/signUp';
+import { errorHandler } from './middlewares/ErrorHandler';
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
