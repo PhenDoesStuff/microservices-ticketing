41fcedc24d7c425d84809c0b0f59dbbd221f63b8
Stephen Montague <Stephen.D.Montague@gmail.com>
2023-06-04 15:24:50 -0400

    Working on jwt things

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { RequestValidationError } from '../errors/RequestValidationError';
 import { User } from '../models/User';
 import { BadRequestError } from '../errors/BadRequestError';
@@ -30,6 +32,20 @@ router.post(
 
 		const user = User.build({ email, password });
 		await user.save();
+
+		// Generate JWT
+		const userJWT = jwt.sign(
+			{
+				id: user.id,
+				email: user.email,
+			},
+			'privateKey'
+		);
+
+		// Store it on the session object
+		req.session = {
+			jwt: userJWT,
+		};
 
 		res.status(201).send(user);
 	}
